26aa492dafbf8bb5eb9e0a3371e6a7e5fc725639
victorHSFerraz <victorf178@gmail.com>
2023-12-16 21:37:57 -0300

    rotating vectors

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	gcc -o renderer -Wall -std=c99 ./src/*.c -IC:/SDL2/include -lSDL2main -lSDL2 -LC:/SDL2/lib/x64
+	gcc -o renderer -Wall -std=c99 ./src/*.c -IC:/SDL2/include -lSDL2main -lSDL2 -lm -LC:/SDL2/lib/x64
 
 run:
 	./renderer.exe
--- src/main.c
+++ src/main.c
@@ -11,7 +11,8 @@
 vec3_t cube_points[N_POINTS];
 vec2_t projected_points[N_POINTS];
 
-vec3_t camera_position = {.x = 0, .y = 0, .z = -3};
+vec3_t camera_position = {.x = 0, .y = 0, .z = -5};
+vec3_t cube_rotation = {.x = 0, .y = 0, .z = 0};
 
 float fov_factor = 640;
 
@@ -73,12 +74,20 @@ vec2_t project(vec3_t point) {
 }
 
 void update(void) {
+    cube_rotation.x += 0.001;
+    cube_rotation.y += 0.001;
+    cube_rotation.z += 0.001;
+
     for (int i = 0; i < N_POINTS; i++) {
         vec3_t point = cube_points[i];
 
-        point.z -= camera_position.z;
+        vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
+        transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
+        transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
 
-        vec2_t projected_point = project(point);
+        transformed_point.z -= camera_position.z;
+
+        vec2_t projected_point = project(transformed_point);
         projected_points[i] = projected_point;
     }
 }
