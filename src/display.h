52161202143d9b6f060b384eacda035979173d1e
victorHSFerraz <victorf178@gmail.com>
2023-12-18 16:21:14 -0300

    drawing triangles

--- src/display.h
+++ src/display.h
@@ -20,6 +20,8 @@ bool initialize_window(void);
 void draw_grid(void);
 void draw_pixel(int x, int y, uint32_t color);
 void draw_rect(int setY, int setX, int width, int height, uint32_t color);
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 void render_color_buffer();
 void clear_color_buffer(uint32_t color);
 void destroy_window(void);
--- src/main.c
+++ src/main.c
@@ -4,12 +4,10 @@
 #include <stdio.h>
 
 #include "display.h"
+#include "mesh.h"
 #include "vector.h"
 
-#define N_POINTS (9 * 9 * 9)
-
-vec3_t cube_points[N_POINTS];
-vec2_t projected_points[N_POINTS];
+triangle_t triangles_to_render[N_MESH_FACES];
 
 vec3_t camera_position = {.x = 0, .y = 0, .z = -5};
 vec3_t cube_rotation = {.x = 0, .y = 0, .z = 0};
@@ -33,21 +31,6 @@ void setup(void) {
         SDL_TEXTUREACCESS_STREAMING,
         window_width,
         window_height);
-
-    int point_count = 0;
-
-    for (float x = -1; x <= 1; x += 0.25) {
-        for (float y = -1; y <= 1; y += 0.25) {
-            for (float z = -1; z <= 1; z += 0.25) {
-                vec3_t new_point = {
-                    .x = x,
-                    .y = y,
-                    .z = z,
-                };
-                cube_points[point_count++] = new_point;
-            }
-        }
-    }
 }
 
 void process_input(void) {
@@ -87,30 +70,54 @@ void update(void) {
     cube_rotation.y += 0.01;
     cube_rotation.z += 0.01;
 
-    for (int i = 0; i < N_POINTS; i++) {
-        vec3_t point = cube_points[i];
+    for (int i = 0; i < N_MESH_FACES; i++) {
+        face_t mesh_face = mesh_faces[i];
 
-        vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
-        transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
-        transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+        vec3_t face_vertices[3];
+        face_vertices[0] = mesh_vertices[mesh_face.a - 1];
+        face_vertices[1] = mesh_vertices[mesh_face.b - 1];
+        face_vertices[2] = mesh_vertices[mesh_face.c - 1];
 
-        transformed_point.z -= camera_position.z;
+        triangle_t projected_triangle;
 
-        vec2_t projected_point = project(transformed_point);
-        projected_points[i] = projected_point;
+        for (int j = 0; j < 3; j++) {
+            vec3_t transformed_vertex = face_vertices[j];
+
+            transformed_vertex = vec3_rotate_x(transformed_vertex, cube_rotation.x);
+            transformed_vertex = vec3_rotate_y(transformed_vertex, cube_rotation.y);
+            transformed_vertex = vec3_rotate_z(transformed_vertex, cube_rotation.z);
+
+            transformed_vertex.z -= camera_position.z;
+
+            vec2_t projected_point = project(transformed_vertex);
+
+            projected_point.x += (window_width / 2);
+            projected_point.y += (window_height / 2);
+
+            projected_triangle.points[j] = projected_point;
+        }
+
+        triangles_to_render[i] = projected_triangle;
     }
 }
 
 void render(void) {
     draw_grid();
 
-    for (int i = 0; i < N_POINTS; i++) {
-        vec2_t projected_point = projected_points[i];
-        draw_rect(projected_point.x + (window_width / 2),
-                  projected_point.y + (window_height / 2),
-                  4,
-                  4,
-                  0xFFFF00FF);
+    for (int i = 0; i < N_MESH_FACES; i++) {
+        triangle_t triangle = triangles_to_render[i];
+
+        draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
+        draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
+        draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFFFF00);
+
+        draw_triangle(triangle.points[0].x,
+                      triangle.points[0].y,
+                      triangle.points[1].x,
+                      triangle.points[1].y,
+                      triangle.points[2].x,
+                      triangle.points[2].y,
+                      0xFF00FF00);
     }
 
     render_color_buffer();
